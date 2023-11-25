af3da59f587e426805933909cbb9ddf592d565a6
Rita <you@example.com>
2023-11-25 14:24:44 -0700

    clean up initial GUI

--- boundary/GUI.java
+++ boundary/GUI.java
@@ -17,9 +17,11 @@ import java.util.ArrayList;
 public class GUI extends JFrame implements ActionListener {
 
     private Database db;
-    private JButton generateButton;
     private JLabel welcome;
     private JLabel instructions;
+    private JButton registerButton;
+    private JButton loginButton;
+    private JButton guestLink;
 
     public GUI(Database db) {
         this.db = db;
@@ -28,164 +30,86 @@ public class GUI extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         Container pane = getContentPane();
-        pane.setLayout(new FlowLayout());
+        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+
         welcome = new JLabel("Welcome to Skyward Bound!");
-        instructions = new JLabel("Please click the button below to generate your schedule for the day.");
+        welcome.setAlignmentX(Component.CENTER_ALIGNMENT);
+        instructions = new JLabel("Please register or login to continue.");
+        instructions.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Panel for buttons and guest link
+        JPanel buttonsAndLinkPanel = new JPanel();
+        buttonsAndLinkPanel.setLayout(new BoxLayout(buttonsAndLinkPanel, BoxLayout.Y_AXIS));
+
+        // Buttons Panel
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+
+        // Register Button
+        registerButton = new JButton("Register");
+        registerButton.addActionListener(this);
+        buttonsPanel.add(registerButton);
+
+        // Login Button
+        loginButton = new JButton("Login");
+        loginButton.addActionListener(this);
+        buttonsPanel.add(loginButton);
+        buttonsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Continue as Guest Link
+        guestLink = new JButton("Continue as Guest");
+        guestLink.setBorderPainted(false);
+        guestLink.setContentAreaFilled(false);
+        guestLink.setFocusPainted(false);
+        guestLink.setOpaque(false);
+        guestLink.setForeground(Color.BLUE);
+        guestLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        // Add underline and action listener
+        guestLink.setFont(new Font(guestLink.getFont().getName(), Font.PLAIN, guestLink.getFont().getSize()));
+        guestLink.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                continueAsGuest();
+            }
+        });
+        guestLink.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Add buttons and link to nested panel
+        buttonsAndLinkPanel.add(buttonsPanel);
+        buttonsAndLinkPanel.add(guestLink);
+
+        // Add components to main pane
         pane.add(welcome);
         pane.add(instructions);
-        generateButton = new JButton("Generate Schedule");
-        generateButton.addActionListener(this);
-        pane.add(generateButton);
+        pane.add(buttonsAndLinkPanel);
         
         setVisible(true);
     }
     
+    private void continueAsGuest() {
+        // Logic to continue as a guest
+        JOptionPane.showMessageDialog(this, "Continuing as guest...");
+    }
+
+    @Override
     public void actionPerformed(ActionEvent e) {
-        
+        if (e.getSource() == registerButton) {
+            // Logic for registering
+            JOptionPane.showMessageDialog(this, "Register functionality not implemented yet.");
+        } else if (e.getSource() == loginButton) {
+            // Logic for logging in
+            JOptionPane.showMessageDialog(this, "Login functionality not implemented yet.");
+        }
     }
     
-    // private boolean backupCalculationAndConfirmation() {
-    //     rescue.backupCalculation();
-    //     if (rescue.getImpossibleHours().size() != 0){
-    //         return false;
-    //     } else if (rescue.getBackupsNeededAtHours().size() != 0) {
-    //         StringBuilder hoursStringBuilder = new StringBuilder();
-    //         for (int hour : rescue.getBackupsNeededAtHours()) {
-    //             if (hoursStringBuilder.length() > 0) {
-    //                 hoursStringBuilder.append(", ");
-    //             }
-    //             hoursStringBuilder.append(hour);
-    //         }
-    //         String hoursString = hoursStringBuilder.toString();
-    //         int choice = JOptionPane.showConfirmDialog(this, "Backup volunteer needed at the following hours: " + hoursString + ".\nIs this okay?", "Confirmation", JOptionPane.YES_NO_OPTION);
-    //         return choice == JOptionPane.YES_OPTION;
-    //     } else {
-    //         return true;
-    //     }
-    // }
-    
-    // private void generateSchedule() {
-    //     try {
-    //         rescue.generateSchedule();
-    //         JOptionPane.showMessageDialog(this, "Schedule generated successfully!");
-    //     } catch (IOException exp) {
-    //         JOptionPane.showMessageDialog(this, "Error writing to file. Please check file permissions.");
-    //     }
-        
-    // }
-    
-    // private void handleImpossible() {
-    //     try {
-    //         FileWriter myWriter = new FileWriter("schedule.txt");
-    //         myWriter.write("Schedule could not be generated.");
-    //         myWriter.close();
-    //         StringBuilder hoursStringBuilder = new StringBuilder();
-    //         for (int hour : rescue.getImpossibleHours()) {
-    //             if (hoursStringBuilder.length() > 0) {
-    //                 hoursStringBuilder.append(", ");
-    //             }
-    //             hoursStringBuilder.append(hour);
-    //         }
-    //         String hoursString = hoursStringBuilder.toString();
-    //         JOptionPane.showMessageDialog(this, "Schedule could not be generated since too many events were scheduled at the times " + hoursString + " and more than one backup would be necessary to account for them.");
- 
-    //     } catch (IOException ex) {
-    //         JOptionPane.showMessageDialog(this, "Error writing to file. Please check file permissions.");
-    //     }
-    // }
-
-    // private void handleScheduleGenerationError() {
-    //     try {
-    //         FileWriter myWriter = new FileWriter("schedule.txt");
-    //         myWriter.write("Schedule could not be generated.");
-    //         myWriter.close();
-    //         int nextChoice = JOptionPane.showConfirmDialog(this, "Schedule could not be generated. Would you like to alter treatment times?", "Confirmation", JOptionPane.YES_NO_OPTION);
-    //         if (nextChoice == JOptionPane.YES_OPTION) {
-    //             handleTreatmentAlteration();
-    //         } else {
-    //             JOptionPane.showMessageDialog(this, "Sorry, schedule generation unsuccessful.");
-    //         }
-    //     } catch (IOException ex) {
-    //         JOptionPane.showMessageDialog(this, "Error writing to file. Please check file permissions.");
-    //     }
-    // }
-    
-    // private void handleTreatmentAlteration() {
-    //     String[] hoursArray = rescue.getBackupsNeededAtHours().stream().map(Object::toString).toArray(String[]::new);
-    //     DefaultComboBoxModel<String> comboBoxModel = new DefaultComboBoxModel<>(hoursArray);
-    //     JComboBox<String> comboBox = new JComboBox<String>(comboBoxModel);
-    //     comboBox.setSelectedIndex(0);
-    //     JLabel messageLabel = new JLabel("Select the hour when backup is not available and click OK.");
-    //     JPanel panel = new JPanel(new GridLayout(0, 1));
-    //     panel.add(messageLabel);
-    //     panel.add(comboBox);
-    //     int result = JOptionPane.showOptionDialog(null, panel, "Select the hour:", JOptionPane.OK_CANCEL_OPTION,
-    //             JOptionPane.PLAIN_MESSAGE, null, null, null);
-    
-    //     while (result == JOptionPane.OK_OPTION) { // check if the user clicked OK
-    //         String selectedHour = (String) comboBox.getSelectedItem(); // get the selected hour
-    //         List<Treatment> treatmentsAtHour = new ArrayList<>(rescue.getTreatments().stream().collect(Collectors.groupingBy(Treatment::getStartHour)).get(selectedHour));
-    //         List<String> treatmentIdStrings = treatmentsAtHour.stream().map(Treatment::getId).collect(Collectors.toList());
-    //         DefaultComboBoxModel<String> comboBoxModel2 = new DefaultComboBoxModel<>(treatmentIdStrings.toArray(new String[0]));
-    //         JComboBox<String> comboBox2 = new JComboBox<String>(comboBoxModel2); // create a JComboBox with the ids array
-    //         comboBox2.setSelectedIndex(0); // set the default selection to the first id in the list
-
-    //         JLabel messageLabel2 = new JLabel("Select the treatment id of the treatment you would like to alter and click OK.");
-    //         JPanel panel2 = new JPanel(new GridLayout(0, 1));
-    //         panel2.add(messageLabel2);
-    //         panel2.add(comboBox2);
-    //         int result2 = JOptionPane.showOptionDialog(null, panel2, "Select the treatment id:", JOptionPane.OK_CANCEL_OPTION,
-    //                 JOptionPane.PLAIN_MESSAGE, null, null, null);
-
-    //         if (result2 == JOptionPane.OK_OPTION) {
-    //             String treatmentIDString = (String) comboBox2.getSelectedItem();
-    //             String newHour = JOptionPane.showInputDialog(this, "Enter the new start hour for this treatment:");
-    //             while (Integer.parseInt(newHour) < 0 || Integer.parseInt(newHour) > 23) {
-    //                 JOptionPane.showMessageDialog(this, "Invalid input. Start hour must be from 0 to 23. Please try again.");
-    //                 newHour = JOptionPane.showInputDialog(this, "Enter the new start hour for this treatment:");
-    //             }
-    //             try {
-    //                 db.updateTreatmentStartHour(Integer.parseInt(treatmentIDString), Integer.parseInt(newHour));
-    //                 for (Treatment t : rescue.getTreatments()) {
-    //                     if (t.getId().equals(treatmentIDString)) {
-    //                         t.setStartHour(newHour);
-    //                         break;
-    //                     }
-    //                 }
-    //             } catch (SQLException ex) {
-    //                 ex.printStackTrace();
-    //             }
-    //             JOptionPane.showMessageDialog(this, "Start hour updated successfully!");
-    //             rescue.backupCalculation();
-    //             if (rescue.getBackupsNeededAtHours().contains(Integer.parseInt(selectedHour))) {
-    //                 JOptionPane.showMessageDialog(this, "More alteration is needed for the hour: " + selectedHour);
-    //                 continue;
-    //             } else {
-    //                 JOptionPane.showMessageDialog(this, "Hour " + selectedHour + " no longer needs backup!");
-    //                 if (backupCalculationAndConfirmation()) {
-    //                     generateSchedule();
-    //                 } else if (rescue.getImpossibleHours().size() != 0) {
-    //                     handleImpossible();
-    //                 } else {
-    //                     handleScheduleGenerationError();
-    //                 }
-    //                 break;
-    //             }
-
-    //         }
-    //     }
-    // }
-    // public static void main(String[] args) {
-    //     try {
-    //         Database db = new Database();
-    //         ArrayList<Animal> animals = db.getAnimalsArrayL();
-    //         ArrayList<Treatment> treatments = db.getTreatmentsArrayL();
-    //         ArrayList<Task> tasks = db.getTasksArrayL();
-    //         ExampleWildlifeRescue rescue = new ExampleWildlifeRescue(animals, tasks, treatments);
-    //         GUI gui = new GUI(rescue, db);
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
+    public static void main(String[] args) {
+        try {
+            Database db = new Database();
+            GUI gui = new GUI(db);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
