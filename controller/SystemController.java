0620cb75016bbc9b1a2653d90f4990f649825dda
thomasmattern2 <thomasmattern1@gmail.com>
2023-11-25 17:57:53 +0000

    init controller

---
+++ controller/SystemController.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class SystemController {
+
+    public SystemController(){
+
+    }
+
+	public void startup() {
+
+	}
+    public static void main(String[] args) {
+		SystemController system = new SystemController();
+	}
+	
+}
