3a596d7d77d47a4990a6d6a4171cb808345e8e66
Thomas Mattern <113630601+ThomasMattern2@users.noreply.github.com>
2023-11-24 19:39:23 -0700

    Delete Domain/flightInformation directory

--- Domain/flightInformation/Airline.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Airline{
-    private String compantName;
-    private ArrayList<Aircraft> aircrafts;
-    private AirlineAgent agent;
-    private ArrayList<CrewMembers> crewMembers;
-
-}
--- Domain/flightInformation/ComfortSeat.java
+++
@@ -1,3 +0,0 @@
-public class ComfortSeat extends Seat{
-    
-}
