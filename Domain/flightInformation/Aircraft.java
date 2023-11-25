ce8e3fa8a1d616b19fb6eec29d6346fd137b3ac4
thomasmattern2 <thomasmattern1@gmail.com>
2023-11-25 00:18:17 +0000

    some changes

--- Domain/flightInformation/Aircraft.java
+++ Domain/flightInformation/Aircraft.java
@@ -1,3 +1,69 @@
+import java.util.ArrayList;
+
 public class Aircraft{
+
+    // HOW DO WE KNOW PRICE?????
     
+    private int id;
+    private String name;
+    Flight assignedFlight;
+    private int capacity;
+    ArrayList<Seat> seats;
+    
+    // Constructor
+    public Aircraft(int id, String name, Flight assignedFlight, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.assignedFlight = assignedFlight;
+        this.capacity = capacity;
+        this.seats = new ArrayList<>();
+
+        // Initialize seats one by one with a max of 120, price default 100?
+        for (int seatNum = 1; seatNum <= capacity; seatNum++) {
+            Seat seat = new Seat(100, seatNum);
+            this.seats.add(seat);
+        }
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Flight getAssignedFlight() {
+        return assignedFlight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public ArrayList<Seat> getSeats() {
+        return seats;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    // methods
+    public void assignFlight(Flight flight){
+        assignedFlight = flight;
+    }
+    // used for when customer books
+    public void changeSeatStatus(boolean status, int seatNum){
+
+    }
 }
--- Domain/flightInformation/Seat.java
+++ Domain/flightInformation/Seat.java
@@ -1,4 +1,46 @@
-public class Seat{
+// Represents a seat in a aircraft
+public class Seat {
 
-    
+    private int price;           // Price of the seat
+    private boolean isAvailable; // Availability status of the seat
+    private int seatNum;         // Seat number
+
+    // Default constructor
+    public Seat() {
+        price = 0;
+        isAvailable = true;
+        seatNum = 1;
+    }
+
+    // Parameterized constructor
+    public Seat(int price, int seatNum) {
+        this.price = price;
+        isAvailable = true;
+        this.seatNum = seatNum;
+    }
+
+    // Reserves the seat, marking it as unavailable
+    public void reserveSeat() {
+        isAvailable = false;
+    }
+
+    // Sets the price of the seat
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    // Gets the price of the seat
+    public int getPrice() {
+        return price;
+    }
+
+    // Checks the availability status of the seat
+    public boolean getAvailability() {
+        return isAvailable;
+    }
+
+    // Gets the seat number
+    public int getSeatNum() {
+        return seatNum;
+    }
 }
