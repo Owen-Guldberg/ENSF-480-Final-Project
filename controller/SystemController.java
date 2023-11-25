4c86144d3bc7323337215f44e7da8d32ae587b8a
Thomas Mattern <113630601+ThomasMattern2@users.noreply.github.com>
2023-11-25 16:46:12 -0700

    Update SystemController.java

--- controller/SystemController.java
+++ controller/SystemController.java
@@ -1,12 +1,27 @@
 package controller;
+import util.*;
+
+import java.util.ArrayList;
+
+import flightInfo.*;
+import role.*;
 
 public class SystemController {
+	private ArrayList<Flight> flights;
+	private ArrayList<Aircraft> aircrafts;
+	private ArrayList<Location> locations;
+	private ArrayList<CrewMember> crewMembers;
+	private ArrayList<RegisteredCustomer> registeredCustomers;
+	private ArrayList<Ticket> tickets;
+	//private ArrayList<Payment> payment;
+
+
     public SystemController(){
 		start();
     }
 
 	public void start() {
-
+		
 	}
 
     public static void main(String[] args) {
