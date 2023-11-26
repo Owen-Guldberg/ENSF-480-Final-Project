963e82d9687b1873335f2b052fda6cf55ea57914
srshendye <100380251+srshendye@users.noreply.github.com>
2023-11-25 17:14:37 -0700

    Update Database.java
    added other methods to populate arraylists

--- database/Database.java
+++ database/Database.java
@@ -7,21 +7,45 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class Database {
-    private Connection dbConnection;
+     private Connection dbConnection;
+    public static Database onlyInstance;
     private ResultSet results;
     private final String URL = "jdbc:mysql://localhost:3306/skyward_bound"; 
     private final String USERNAME = "root";
-    private final String PASSWORD = "";
-
-    private ArrayList<RegisteredCustomer> registeredUsers = new ArrayList<RegisteredCustomer>();
+    private final String PASSWORD = "Ravenclaw23";
+    public ArrayList<Loc> locations = new ArrayList<Loc>();
+    public ArrayList<RegisteredCustomer> registeredUsers = new ArrayList<RegisteredCustomer>();
+    public ArrayList<Aircraft> aircrafts = new ArrayList<Aircraft>(); 
+    public ArrayList<Flight> flights = new ArrayList<Flight>(); 
+    public ArrayList<AirlineAgent> crew = new ArrayList<AirlineAgent>(); 
+    public ArrayList<Ticket> crew = new ArrayList<Ticket>();
+    
 
 /**
  * Constructs a new Database object and connects to the database specified
  * by the URL, username, and password instance variables.
  * @throws SQLException if there is an error connecting to the database
  */
-    public Database() throws SQLException {
+    private Database() throws SQLException {
         this.dbConnection = connectToDatabase();
+        getLocationData();
+        readRegisteredUsers();
+        getAircraftData();
+        getFlightData();
+        getCrewMemberData(); 
+    }
+
+    public static Database getOnlyInstance(){
+        if(onlyInstance == null){
+            try{
+                onlyInstance = new Database();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            
+        }
+        return onlyInstance; 
     }
 
 /**
@@ -41,7 +65,7 @@ public class Database {
     }
 
 
-// /**
+// /** WORKS
 //  * Reads Registered User data from the database and populates the registeredUsers ArrayList.
 //  * @throws SQLException if there is an error reading data from the database
 //  */
@@ -68,63 +92,138 @@ public class Database {
         }
     }
 
-// /**
-//  * Reads treatments table from the database and populates the treatments ArrayList.
-//  * @throws SQLException if there is an error executing the SQL statement
-//  */
-//     public void readTreatmentsFromDatabase() throws SQLException {
-//     try {
-//         Statement stmt = dbConnection.createStatement();
-//         String query = "SELECT TreatmentID, AnimalID, TaskID, StartHour FROM treatments";
-//         ResultSet results = stmt.executeQuery(query);
+    ///WORKS
+    public void getLocationData(){
+        try{
+            Statement myStmt = this.dbConnection.createStatement();
+            String query = "SELECT * FROM LOCATIONS";
+            results = myStmt.executeQuery(query);
+            while(results.next()){
+              int locID = results.getInt("LocationID");
+              String airportName= results.getString("AirportName");
+              String city = results.getString("City");
+              String country = results.getString("Country");
+    
+              Loc tmp = new Loc(airportName, city, country); //creates food item with each item in the database
+              this.locations.add(tmp); //stores food item in linkedlist of food objects
+              }
+              myStmt.close(); 
+            }catch (SQLException ex) {
+                //ex.printStackTrace();
+             }
+    }
 
-//         while (results.next()) {
-//             String id = results.getString("TreatmentID");
-//             String animalID = results.getString("AnimalID");
-//             String taskID = results.getString("TaskID");
-//             String startHour = results.getString("StartHour");
-//             treatments.add(new Treatment(id, animalID, taskID, startHour));
-//         }
-//         stmt.close();
-//     }
-//     catch(SQLException e){
-//         e.printStackTrace();
-//     }
-// }
+    
 
-// /**
-//  * Reads tasks table from the database and populates the tasks ArrayList.
-//  * @throws SQLException if there is an error executing the SQL statement
-//  */
-//     public void readTasksFromDatabase() throws SQLException {
-//         try{
-//             Statement stmt = dbConnection.createStatement();
-//             String query = "SELECT TaskID, Description, Duration, MaxWindow FROM tasks";
-//             ResultSet results = stmt.executeQuery(query);
+    ///WORKS
+    public void getAircraftData(){
+        try{
+            Statement myStmt = this.dbConnection.createStatement();
+            String query = "SELECT * FROM AIRCRAFT";
+            results = myStmt.executeQuery(query);
+            while(results.next()){
+              int airID = results.getInt("AircraftID");
+              String airName= results.getString("Name");
+              int capacity = results.getInt("Capacity");
+    
+              Aircraft tmp = new Aircraft(airID, airName, capacity); //creates 
+              this.aircrafts.add(tmp); //stores
+              }
+              myStmt.close(); 
+            }catch (SQLException ex) {
+                //ex.printStackTrace();
+             }
 
-//             while (results.next()) {
-//                 String id = results.getString("TaskID");
-//                 String description = results.getString("Description");
-//                 int duration = results.getInt("Duration");
-//                 int maxWindow = results.getInt("MaxWindow");
-//                 tasks.add(new Task(id, description, duration, maxWindow));
-//             }
-//             stmt.close();
-//         }
-//         catch(SQLException e){
-//             e.printStackTrace();
-//         }
-//     }
+    }
 
-//     public void updateTreatmentStartHour(int treatmentId, int newStartHour) throws SQLException {
-//         String query = "UPDATE treatments SET startHour = ? WHERE TreatmentID = ?";
-//         PreparedStatement statement = dbConnection.prepareStatement(query);
-//         statement.setInt(1, newStartHour);
-//         statement.setInt(2, treatmentId);
-//         int rowsAffected = statement.executeUpdate();
-//         System.out.println(rowsAffected + " rows updated.");
-//         statement.close();
-//     }
+
+    public void getFlightData(){
+        try{
+            Statement myStmt = this.dbConnection.createStatement();
+            String query = "SELECT * FROM FLIGHTS";
+            ResultSet results = myStmt.executeQuery(query);
+            while(results.next()){
+              int id = results.getInt("FlightID");
+              String flightNum = results.getString("FlightNum");
+              String flightDate = results.getString("FlightDate");
+              String originName = results.getString("Origin");
+              String destinationName = results.getString("Destination");
+              String aircraft= results.getString("Aircraft");
+              String departTime = results.getString("DepartureTime");
+              String arriveTime = results.getString("ArrivalTime");
+              String flightTime = results.getString("FlightTime");
+
+              Loc origin = new Loc("gay", "gay", "gay");
+              Loc destination = new Loc("gay", "gay", "gay"); 
+              for (int i = 0; i < locations.size(); i++){
+                if(locations.get(i).getAirportName().equals(originName) == true){
+                  origin = locations.get(i); 
+                }
+                else if(locations.get(i).getAirportName().equals(destinationName) == true){
+                  destination = locations.get(i); 
+                }
+              }
+
+              Aircraft a = new Aircraft();
+              for (int i = 0; i < aircrafts.size(); i++){
+                if(aircrafts.get(i).getName().equals(aircraft) == true){
+                    a = aircrafts.get(i); 
+                }
+              }
+
+            //   public Flight(Loc origin, Loc destination, String flightNum, String departureTime, String arrivalTime, String flightTime, Aircraft aircraft)
+               
+
+              Flight tmp= new Flight(origin,destination, flightNum, flightDate, departTime, arriveTime, flightTime, a); //creates food item with each item in the database
+              this.flights.add(tmp); 
+              //this.inventory.add(foodItem); //stores food item in linkedlist of food objects
+              
+            }
+            myStmt.close(); 
+            }catch (SQLException ex) {
+                //ex.printStackTrace();
+             }
+             
+    }
+
+
+    public void getCrewMemberData(){
+
+        try{
+            Statement myStmt = this.dbConnection.createStatement();
+            String query = "SELECT * FROM CREWMEMBER";
+            results = myStmt.executeQuery(query);
+            while(results.next()){
+              int crewID = results.getInt("CrewID");
+              String FName= results.getString("FName");
+              String LName= results.getString("LName");
+              String email= results.getString("Email");
+              String job= results.getString("Job");
+              int house = results.getInt("HouseNum");
+              String street= results.getString("Street");
+              String city = results.getString("City");
+              String country = results.getString("Country");
+              String postal= results.getString("PostalCode");
+    
+              Name n = new Name(FName, LName);//creates food item with each item in the database
+              Address ad = new Address(house, street, city, country, postal); //dont need but maybe could add to airline agent if needed
+
+
+                AirlineAgent tmp = new AirlineAgent(n, email); 
+                crew.add(tmp); 
+              }
+              myStmt.close(); 
+            }catch (SQLException ex) {
+                //ex.printStackTrace();
+             }
+
+    }
+
+    public void getTicketData(){
+        //need to clarify logic for tickets in db
+    }
+
+//
 
     /**
      * Gets the URL of the database.
@@ -149,36 +248,6 @@ public class Database {
     public String getPassword(){
         return this.PASSWORD;
     }
-
-//     /**
-//      * Getter for ArrayList containing all the animal data from the database.
-//      * @return an ArrayList containing all the animal data from the database
-//      * @throws SQLException if there is an error executing the SQL statement
-//      */
-//     public ArrayList<Animal> getAnimalsArrayL() throws SQLException {
-//         readAnimalsFromDatabase();
-//         return this.animals;
-//     }
-
-//     /**
-//     * Getter for ArrayList containing all the task data from the database.
-//     * @return an ArrayList containing all the task data from the database
-//     * @throws SQLException if there is an error executing the SQL statement
-//     */
-//     public ArrayList<Task> getTasksArrayL() throws SQLException {
-//         readTasksFromDatabase();
-//         return this.tasks;
-//     }
-
-//     /**
-//      * Getter for ArrayList containing all the treatment data from the database.
-//      * @return an ArrayList containing all the treatment data from the database
-//      * @throws SQLException if there is an error executing the SQL statement
-//      */
-//     public ArrayList<Treatment> getTreatmentsArrayL() throws SQLException {
-//         readTreatmentsFromDatabase();
-//         return this.treatments;
-//     }
 
 //     /**
 //     * Closes the database connection and result set.
