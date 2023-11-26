c6ebf1c5c61f1afd533fb155883f3bb4557cce5c
thomasmattern2 <thomasmattern1@gmail.com>
2023-11-26 17:38:50 +0000

    fixed auth

--- controller/AuthenticationController.java
+++ controller/AuthenticationController.java
@@ -2,15 +2,21 @@ package controller;
 
 import role.*;
 
+import java.util.ArrayList;
+
+import database.*;
+
 public class AuthenticationController {
     private RegisteredCustomer user;
+    private Database db;
 
-    public AuthenticationController(RegisteredCustomer user) {
+    public AuthenticationController(RegisteredCustomer user, Database db) {
         this.user = user;
+        this.db = db;
     }
 
-    public boolean loginUser(String username, String password) {
-        RegisteredCustomer user = user.getUserByUsername(username);
+    public boolean loginUser(String email, String password) {
+
         if (user != null && user.getPassword().equals(password)) {
             // Login successful
             return true;
@@ -19,11 +25,14 @@ public class AuthenticationController {
     }
 
     public boolean registerUser(RegisteredCustomer user) {
-        if (user.getUserByUsername(user.getUsername()) != null) {
-            // User already exists
-            return false;
+        ArrayList<RegisteredCustomer> passengerData = db.getRegisteredCustomerData();
+        for(RegisteredCustomer passenger : passengerData){
+            if(user == passenger){
+                return false;
+            }
         }
-        user.saveUser(user);
+        // user doesnt exsist in RegisteredCustomer
+        db.saveUser(user);
         return true;
     }
 }
