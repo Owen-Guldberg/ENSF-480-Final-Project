ffe47b4caeed4c8db263e85f878ef9357085580f
thomasmattern2 <thomasmattern1@gmail.com>
2023-11-27 21:53:42 +0000

    payment added some promo codes from email

--- controller/paymentController.java
+++ controller/paymentController.java
@@ -1,10 +1,28 @@
 package controller;
 
-public class paymentController {
-    public paymentController(){}
-    
-    public setOwed(){
+import util.*;
 
+import java.util.ArrayList;
+
+
+public class PaymentController {
+
+    public PaymentController() {}
+
+    public void setStrat(String promoCode, Payment payment) {
+        if ("MEMBER15".equals(promoCode)) {
+            fifteenDiscount disc = new fifteenDiscount();
+            double discountedPrice = disc.applyDiscount(payment.getAmountOwed());
+            payment.setOwed(discountedPrice);
+        }
+        else if("MEMBER50".equals(promoCode)){
+            fiftyDiscount disc = new fiftyDiscount();
+            double discountedPrice = disc.applyDiscount(payment.getAmountOwed());
+            payment.setOwed(discountedPrice);
+        }
     }
 
+    public void setTicketPrice(double price, Payment userPayment) {
+        userPayment.setOwed(price);
+    }
 }
