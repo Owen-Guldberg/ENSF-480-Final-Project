0709db8a4f65ba9891a417f302e819c5ddb80d06
Owen <owen.guldberg@ucalgary.ca>
2023-11-28 07:14:02 -0700

    added jar for sending gmails and send promo code

--- controller/AuthenticationController.java
+++ controller/AuthenticationController.java
@@ -4,6 +4,8 @@ import role.*;
 
 import java.util.ArrayList;
 
+import com.owen_guldberg.gmailsender.GMailer;
+
 import database.*;
 import util.*;
 
@@ -33,6 +35,25 @@ public class AuthenticationController {
         // Creating RegisteredCustomer object
         Name customerName = new Name(firstName, lastName);
         RegisteredCustomer newCustomer = new RegisteredCustomer(customerName, email, password, address, null); // Assuming null for Payment
+        
+
+        // Send user a promotion code via gmail for signing up
+        // First time running app will be prompted to login to the gmail acc in browser
+        // user: skywardboundpromotions@gmail.com
+        // pass: skyward_bound1
+        try {
+        GMailer gMailer = new GMailer();
+        gMailer.sendMail(email,"Thank you for registering with Skyward Bound!",
+                "As a token of our appreciation, we're delighted to offer you a special promotion. Use the promo code below for 10% of your first flight!\r\n" +
+                "\r\n" +
+                "Promo Code: FIRSTFLIGHT\r\n" +
+                "\r\n" +
+                "We look forward to serving you on board and providing you with a great travel experience.\r\n" +
+                "Best regards, \r\n" +
+                "The Skyward Bound Team");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // Attempt to register the user in the database
         return registerUser(newCustomer);
