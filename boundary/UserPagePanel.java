1d284d8437e65a59ba2b764bb83052cc90d4ee6a
Rita <you@example.com>
2023-11-30 09:21:02 -0700

    minor things that won't do yet

--- boundary/UserPagePanel.java
+++ boundary/UserPagePanel.java
@@ -2,7 +2,6 @@ package boundary;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
-
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -19,28 +18,28 @@ public class UserPagePanel extends JPanel {
     public UserPagePanel(String name, ActionListener viewMyFlightsListener, ActionListener viewFlightsListener, ActionListener logoutListener, ArrayList<String> locationStrings) {
         this.user = name;
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBackground(Color.WHITE);
         initializeComponents(viewMyFlightsListener, viewFlightsListener, logoutListener, locationStrings);
     }
 
     private void initializeComponents(ActionListener viewMyFlightsListener, ActionListener viewFlightsListener, ActionListener logoutListener, ArrayList<String> locationStrings) {
         // Welcome Label
         JLabel welcomeLabel = new JLabel("Welcome, " + (user.isEmpty() ? "Guest" : user) + "!");
-        welcomeLabel.setFont(new Font(welcomeLabel.getFont().getName(), Font.PLAIN, 20));
+        welcomeLabel.setFont(new Font(welcomeLabel.getFont().getName(), Font.BOLD, 20));
         welcomeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(Box.createVerticalStrut(20));
         add(welcomeLabel);
-        add(Box.createVerticalStrut(20));
 
         // View Flights Button
         if (!user.isEmpty()) {
-            JButton myFlightsButton = new JButton("View My Flights");
+            JButton myFlightsButton = styleButton(new JButton("View My Flights"));
             myFlightsButton.addActionListener(viewMyFlightsListener);
-            myFlightsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
             add(myFlightsButton);
             add(Box.createVerticalStrut(10));
         }
 
         JLabel enterLabel = new JLabel("Please select an origin and a destination below.");
+        enterLabel.setFont(new Font(enterLabel.getFont().getName(), Font.PLAIN, 16));
         enterLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(enterLabel);
         add(Box.createVerticalStrut(10));
@@ -48,35 +47,50 @@ public class UserPagePanel extends JPanel {
         // Location selection
         originList = new JList<>(locationStrings.toArray(new String[0]));
         originList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        originScrollPane = new JScrollPane(originList);
-        TitledBorder titledBorder = BorderFactory.createTitledBorder("Origin Locations");
-        originScrollPane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10), titledBorder));
+        originScrollPane = createScrollableList(originList, "Origin Locations");
 
         destinationList = new JList<>(locationStrings.toArray(new String[0]));
         destinationList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        destinationScrollPane = new JScrollPane(destinationList);
-        TitledBorder titledBorder2 = BorderFactory.createTitledBorder("Destination Locations");
-        destinationScrollPane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10), titledBorder2));
+        destinationScrollPane = createScrollableList(destinationList, "Destination Locations");
 
         JPanel locationSelectionPanel = new JPanel();
-        locationSelectionPanel.setLayout(new FlowLayout(FlowLayout.CENTER)); 
+        locationSelectionPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        locationSelectionPanel.setBackground(Color.WHITE);
         locationSelectionPanel.add(originScrollPane);
         locationSelectionPanel.add(destinationScrollPane);
         add(locationSelectionPanel);
         add(Box.createVerticalStrut(10));
 
-        viewFlightsButton = new JButton("View Available Flights");
+        viewFlightsButton = styleButton(new JButton("View Available Flights"));
         viewFlightsButton.addActionListener(viewFlightsListener);
-        viewFlightsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(viewFlightsButton);
         add(Box.createVerticalStrut(10));
 
         // Logout Button
-        logoutButton = new JButton(user.isEmpty() ? "Return to Home Page" : "Logout");
+        logoutButton = styleButton(new JButton(user.isEmpty() ? "Return to Home Page" : "Logout"));
         logoutButton.addActionListener(logoutListener);
-        logoutButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(logoutButton);
         add(Box.createVerticalStrut(20));
+    }
+
+    private JButton styleButton(JButton button) {
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setBackground(new Color(0, 102, 204)); // Blue background
+        button.setForeground(Color.WHITE); // White text
+        button.setFocusPainted(false);
+        button.setFont(new Font(button.getFont().getName(), Font.BOLD, 16));
+        button.setOpaque(true);
+        button.setBorderPainted(false);
+        button.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        return button;
+    }
+
+    private JScrollPane createScrollableList(JList<String> list, String title) {
+        JScrollPane scrollPane = new JScrollPane(list);
+        scrollPane.setPreferredSize(new Dimension(400, 200));
+        TitledBorder titledBorder = BorderFactory.createTitledBorder(title);
+        scrollPane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10), titledBorder));
+        return scrollPane;
     }
 
     public String getSelectedOrigin() {
